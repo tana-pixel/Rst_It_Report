324855e20c42441a64352bb6beeb0e0d16655ebd
Tana <tana.pittayatanadol@gmail.com>
2026-08-23 08:31:04 +0700

    Delete rst-ecount-sync directory

--- rst-ecount-sync/test.gs.js
+++
@@ -1,5 +0,0 @@
-function triggerPermission() {
-  // บรรทัดนี้จะบังคับให้ระบบ Google ตรวจสอบสิทธิ์ยิง API ใหม่ทั้งหมด
-  var res = UrlFetchApp.fetch("https://oapi.ecount.com");
-  Logger.log(res.getResponseCode());
-}
